ceb5ee7e039a756a20d63c8f83d5d1a67bfa3785
Sean Lucino <sean.lucino1@gmail.com>
2025-10-13 09:09:19 +0800

    model dataset and local train setup done

---
+++ trainmodel.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+from multiprocessing import Process, freeze_support, set_start_method
+
+# fixes RuntimeError where an attempt ... bootstrapping phase.
+
+if __name__ == '__main__':
+    #model = YOLO("yolo11s.pt")
+
+    # Train the model 
+    # results = model.train(data="data.yaml", epochs=30, imgsz=640)
+
+    # Run inference with the YOLO11s model on the 'bus.jpg' image
+    # For testing model
+    
+
+    # Evaluate the model's performance on the validation set
+    best = YOLO("runs/detect/train/weights/best.pt")
+    # results = best("testvideo.mp4", save=True, show=True)
+    results = best.val()
+    print(results.box.map)
+
+# Export the model to ONNX format
+# success = model.export(format="onnx")
